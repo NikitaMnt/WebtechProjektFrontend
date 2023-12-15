d29154979dcf4d7caba4097daec58b05a80c3354
NikitaMnt <nikita.mantel@student.htw-berlin.de>
2023-12-15 13:20:44 +0100

    4. Meilenstein + Anpassungen neu

--- src/views/ToDoView.vue
+++ src/views/ToDoView.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="container">
     <h1>ToDo-Liste</h1>
+
+    <!-- Formular zum Hinzufügen von ToDos -->
+    <form @submit.prevent="addTodo">
+      <input v-model="newTodo" placeholder="Neues ToDo hinzufügen" />
+      <button type="submit">Hinzufügen</button>
+    </form>
+
     <table class="todo-table">
       <thead>
       <tr>
         <th>Tätigkeit</th>
         <th>Erledigt</th>
+        <th>Aktionen</th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="todo in todos" :key="todo.id">
+      <tr v-for="(todo, index) in todos" :key="todo.id">
         <td>{{ todo.taetigkeit }}</td>
-        <td><span v-if="todo.erledigt">☑</span></td>
+        <td>
+          <input type="checkbox" :checked="todo.erledigt" @click="updateTodo(index)">
+        </td>
+        <td>
+          <!-- Button zum Löschen von ToDos -->
+          <button @click="removeTodo(index)">Löschen</button>
+        </td>
       </tr>
       </tbody>
     </table>
@@ -23,15 +37,52 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
 const todos = ref([]);
+const newTodo = ref('');
 
-onMounted(async () => {
+const fetchTodos = async () => {
   try {
     const response = await axios.get('http://localhost:8080/todos');
     todos.value = response.data;
   } catch (error) {
     console.error(error);
   }
-});
+};
+
+const addTodo = async () => {
+  try {
+    const response = await axios.post('http://localhost:8080/todos', { taetigkeit: newTodo.value, erledigt: false });
+    todos.value.push(response.data);
+    newTodo.value = '';
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const removeTodo = async (index) => {
+  try {
+    const todo = todos.value[index];
+    console.log(`Deleting todo with id: ${todo.id}`); // Ausgabe der ID des zu löschenden ToDos
+    const response = await axios.delete(`http://localhost:8080/todos/${todo.id}`);
+    console.log(`Server response: ${response}`); // Ausgabe der Antwort des Servers
+    await fetchTodos(); // Ruft die Liste der ToDos erneut ab
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateTodo = async (index) => {
+  try {
+    const todo = todos.value[index];
+    todo.erledigt = !todo.erledigt; // Wechselt den erledigt-Status
+    const response = await axios.put(`http://localhost:8080/todos/${todo.id}`, todo);
+    console.log(`Server response: ${response}`); // Ausgabe der Antwort des Servers
+    await fetchTodos(); // Ruft die Liste der ToDos erneut ab
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(fetchTodos);
 </script>
 
 <style scoped>
